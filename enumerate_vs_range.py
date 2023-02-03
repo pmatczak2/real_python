fc1712f3507ed881e276c91b875faaa76a97521a
peteykienzzz <pmatczak2@gmail.com>
2023-02-03 09:00:05 -0800

    iterating through a list using range, and enumerates also doctest

--- enumerate_vs_range.py
+++ enumerate_vs_range.py
@@ -4,14 +4,21 @@ def fizz_buzz(numbers):
     1. Replace all integers that are evenly divisible by 3 with "fizz"
     2. Replace all integers divisible by 5 with "buzz"
     3. Replace all integers divisible by both 3 and 5 with "fizzbuzz"
-    numbers = [45, 22, 14, 65, 97, 72]
-    fizz_buzz(numbers)
-    numbers
+    >>> numbers = [45, 22, 14, 65, 97, 72]
+    >>> fizz_buzz(numbers)
+    >>> numbers
     ['fizzbuzz', 22, 14, 'buzz', 97, 'fizz']
     '''
 
-    for i in range(len(numbers)):
-        num = numbers[i]
+    # for i in range(len(numbers)):
+    #     num = numbers[i]
+    #     if num % 3 == 0:
+    #         numbers[i] = "fizz"
+    #     if num % 5 == 0:
+    #         numbers[i] = "buzz"
+    #     if num % 3 == 0 and num % 5 == 0:
+    #         numbers[i] = "fizzbuzz"
+    for i, num in enumerate(numbers):
         if num % 3 == 0:
             numbers[i] = "fizz"
         if num % 5 == 0:
@@ -20,6 +27,9 @@ def fizz_buzz(numbers):
             numbers[i] = "fizzbuzz"
 
 
+
 numbers = [45, 22, 14, 65, 97, 72]
 fizz_buzz(numbers)
 print(numbers)
+
+# running doc test in terminal python3 -m doctest (name of file),
