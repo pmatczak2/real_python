587d1f771b10bcbfdd08d8d8c0256a0d8fd15987
peteykienzzz <pmatczak2@gmail.com>
2023-02-24 06:37:52 -0800

    examples for lambda

--- list_comprehensions_and_list_functions.py
+++ list_comprehensions_and_list_functions.py
@@ -10,7 +10,35 @@ print(item)
 lst = [1,2,-5,6]
 
 
+result = []
 
+# lambda arguments, create a lambda function to add 5 to a gice number
+add_five = lambda x: x+5
 
+answer = add_five(3)
+print(f"\n{answer}")
 
+# lambda with multiple arguments
+# create a lambda function to multiply tow numbers
+multiply = lambda x, y: x * y
+
+# lambda with map()
+# create a list with numbers
+nums = [1,2,3]
+
+# map a lambda function  to add 5 to each number in a list
+mapped = map(lambda x: x+5, nums)
+
+# convert map object to a list
+outcome = list(mapped)
+print(f"\n{outcome}")
+
+# lambda with sorted()
+# create a list of ruples with furits and prices
+fruit_prices = [('banana', 3), ("apple", 1), ("orange", 5)]
+
+# stort the list based on the prices of the fruit
+sorted_prices = sorted(fruit_prices, key=lambda x: x[1])
+
+print(f"\n{sorted_prices}")
 
